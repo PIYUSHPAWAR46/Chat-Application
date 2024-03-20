34001e9fef9f39b9812c04081b7b2704db5c3cfa
PIYUSHPAWAR46 <piyushpawar46@gmail.com>
2024-03-20 17:09:10 +0530

    server error

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ import {app, server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
 
- const __dirname =  path.resolve();
+const __dirname =  path.resolve();
 
 dotenv.config();
 
 app.use(express.json());  //parse in json incoming request (from req.body in auth.controller file)
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users",userRoutes);
 
